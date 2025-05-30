1657fd381dbb6f1daaf56b342054e06e044acd1b
kaufusitm <kaufusitm@go.byuh.edu>
2025-05-30 10:55:25 -1000

    5/30/2025

--- Activity-04/answer-iso.ps1
+++
@@ -1,7 +0,0 @@
-# Builds answer.iso containing only Autounattend.xml
-$iso = "answer.iso"
-$xml = "Autounattend.xml"
-if (-Not (Test-Path $xml)) { Write-Error "Missing $xml"; exit 1 }
-$oscdimg = "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\amd64\Oscdimg\oscdimg.exe"
-& $oscdimg -u2 -udfver102 -lANS -m $xml $iso
-Write-Host "✅ Created $iso"
